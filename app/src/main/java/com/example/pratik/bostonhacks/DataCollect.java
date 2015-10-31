22d832407bd71d90fbfebba66d66bceaf7eae41c
Elvin <elvinw01@gmail.com>
2015-10-31 17:46:00 -0400

    Added DataCollect.java and values xmls

---
+++ app/src/main/java/com/example/pratik/bostonhacks/DataCollect.java
@@ -0,0 +1,26 @@
+package bostonhacks.drinkmate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataCollect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_collect);
+
+//        List age = new ArrayList<Integer>();
+//        for (int i = 1; i <= 100; i++) {
+//            age.add(Integer.toString(i));
+//        }
+//        ArrayAdapter<Integer> spinnerArrayAdapter = new ArrayAdapter<Integer>(
+//                this, android.R.layout.simple_spinner_item, age);
+//        spinnerArrayAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+//
+//        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+//        spinner.setAdapter(spinnerArrayAdapter);
+
+
+    }
+}
