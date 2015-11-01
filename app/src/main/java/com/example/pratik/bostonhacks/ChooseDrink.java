47a94b6b9419188e01d31264726797bab8d34e11
Elvin <elvinw01@gmail.com>
2015-11-01 01:47:48 -0500

    Merge branch 'master' of /Users/elvinw/AndroidStudioProjects/BostonHacks-ATracker with conflicts.

---
+++ app/src/main/java/com/example/pratik/bostonhacks/ChooseDrink.java
@@ -0,0 +1,14 @@
+package com.example.pratik.bostonhacks;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ChooseDrink extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_drink2);
+    }
+
+}
