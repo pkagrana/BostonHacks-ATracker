581f132827ec538031ef85308701c600229e8c00
pkagrana <super123pratik@gmail.com>
2015-11-01 01:26:52 -0500

    Update Nov 1st - 1:25 AM

--- app/src/main/java/com/example/pratik/bostonhacks/DataCollect.java
+++ app/src/main/java/com/example/pratik/bostonhacks/DataCollect.java
@@ -4,15 +4,100 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
 
 public class DataCollect extends Activity {
+
+    int weight;
+    int amount;
+    int age;
+    int minutes;
+    Button beerButton;
+    Button wineButton;
+    Button liquorButton;
+    Button getBLA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_collect);
-    }
+        amount = getAlcoholAmount();
+        age = getUserAge();
+        weight = getUserWeight();
+        minutes = getLastTimeAmount();
+
+        beerButton = (Button) findViewById(R.id.beer);
+        beerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("BEER");
+                System.out.println(getAlcoholAmount());
+                System.out.println(getUserAge());
+                System.out.println(getUserWeight());
+                System.out.println(getLastTimeAmount());
+                Beer b = new Beer (age, weight, amount, minutes, true);
+                double temp = b.getBAC();
+                if (temp > 0.08) {
+
+                }
+            }
+        });
+
+        wineButton = (Button) findViewById(R.id.wine);
+        wineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("WINE");
+                System.out.println(getAlcoholAmount());
+                System.out.println(getUserAge());
+                System.out.println(getUserWeight());
+                System.out.println(getLastTimeAmount());
+                Wine w = new Wine (age, weight, minutes, amount, true);
+                double temp = w.getBAC();
+                if (temp > 0.08) {
+
+                }
+            }
+        });
+
+        liquorButton = (Button) findViewById(R.id.liquor);
+        liquorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("LIQUOR");
+                System.out.println(getAlcoholAmount());
+                System.out.println(getUserAge());
+                System.out.println(getUserWeight());
+                System.out.println(getLastTimeAmount());
+                Liquor l = new Liquor (age, weight, minutes, amount, true);
+                double temp = l.getBAC();
+                if (temp > 0.08) {
+
+                }
+            }
+        });
+
+        getBLA = (Button) findViewById(R.id.getBLA);
+        getBLA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+    } // end of method
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,5 +119,29 @@ public class DataCollect extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    } // end of method
+
+    // Method to get amount of alcohol
+    public int getAlcoholAmount () {
+        EditText temp = (EditText) findViewById(R.id.amount);
+        return Integer.parseInt(temp.getText().toString());
     }
-}
+
+    // Method to get the user age
+    public int getUserAge () {
+        EditText temp = (EditText) findViewById(R.id.age);
+        return Integer.parseInt(temp.getText().toString());
+    }
+
+    // Method to get the userInputedWeight
+    public int getUserWeight () {
+        EditText temp = (EditText) findViewById(R.id.weight);
+        return Integer.parseInt(temp.getText().toString());
+    }
+
+    // Method to get the amount since the last drink
+    public int getLastTimeAmount () {
+        EditText temp = (EditText) findViewById(R.id.time);
+        return Integer.parseInt(temp.getText().toString());
+    }
+} // end of class
